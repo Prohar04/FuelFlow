7f8844f315726b5bd249843701d92d9189849647
Prohar Saha Polak <prohar.saha.polak@g.bracu.ac.bd>
2025-12-29 20:00:12 +0600

    feat: Add index file to export all admin pages

---
+++ client/src/pages/admin/index.js
@@ -0,0 +1,5 @@
+// Export all admin pages
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as PumpsPage } from './PumpsPage';
+export { default as EmployeesPage } from './EmployeesPage';
+export { default as PricesPage } from './PricesPage';
