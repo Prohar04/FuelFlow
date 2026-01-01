25b53a4755e412356b01386dde5c4fe8fe9e0646
Prohar Saha Polak <prohar.saha.polak@g.bracu.ac.bd>
2026-01-01 14:27:42 +0600

    feat: Create index file to export manager pages and skeleton features

---
+++ client/src/pages/manager/index.js
@@ -0,0 +1,15 @@
+// Export all manager pages
+export { default as ManagerDashboard } from './ManagerDashboard';
+export { default as EmployeesPage } from './ManagerEmployeesPage';
+export { default as AttendancePage } from './AttendancePage';
+export { default as ShiftsPage } from './ShiftManagementPage';
+export { default as PayrollPage } from './PayrollPage';
+export { default as SuppliersPage } from './SupplierPage';
+
+// Skeleton pages for features not yet implemented
+export {
+  InventoryPage,
+  OrdersPage,
+  AnalyticsPage,
+  ReportsPage
+} from '../SkeletonPages';
